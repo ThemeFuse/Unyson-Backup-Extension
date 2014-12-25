ff6aa63090a07cc15adc423290d9c369e7f3ff26
moldcraft <moldcraft@email.com>
2014-12-25 16:17:35 +0200

    Updated description

--- manifest.php
+++ manifest.php
@@ -5,7 +5,7 @@
 $manifest = array();
 
 $manifest['name']        = __( 'Backup', 'fw' );
-$manifest['description'] = __( 'This extension lets you set up daily, weekly or monthly backup schedule. You will have the option to choose between a full backup or a data base only backup.', 'fw' );
+$manifest['description'] = __( 'This extension lets you set up daily, weekly or monthly backup schedule. You can choose between a full backup or a data base only backup.', 'fw' );
 $manifest['version'] = '1.0.0';
 $manifest['display'] = true;
 $manifest['standalone'] = true;
